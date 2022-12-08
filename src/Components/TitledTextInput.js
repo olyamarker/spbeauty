3edac5794ea41fec369c0b41366277f0614842b1
Krasnova Olya <67010509+olyamarker@users.noreply.github.com>
2022-12-08 13:01:08 +0000

    Обновила TitledTextInput (чек-лист)

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #0F172A;
     &:hover {
         background-color: #D9E2EFB8;
     }
@@ -14,11 +14,11 @@ const Input = styled.input`
 function TitledTextInput({title, def}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption caption-color'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='p-2' defaultValue={def} ></Input>
+            <Input className='px-3 py-2' defaultValue={def} ></Input>
         </div>
     );
 }
