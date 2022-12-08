5010719d074c9a4d84fa76a10294eaf89f9394be
Krasnova Olya <67010509+olyamarker@users.noreply.github.com>
2022-12-08 13:24:21 +0000

    Обновила ListElement (чек-лист)

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -4,20 +4,16 @@ import EditIcon from '../../../../../Icons/EditIcon';
 import RemoveIcon from '../../../../../Icons/RemoveIcon';
 import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 
-const Option = styled.div`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
-`;
+const Option = styled.div``;
 
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
+            <div className='d-flex gap-1'>
+                <IconOnlyButton icon={<EditIcon/>}/>
+                <IconOnlyButton icon={<RemoveIcon/>}/>
+            </div>
         </div>
     );
 }
