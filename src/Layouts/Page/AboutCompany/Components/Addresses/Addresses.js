2a645ceb4329c75c9d7110900b3401d5ca34d59e
Krasnova Olya <67010509+olyamarker@users.noreply.github.com>
2022-12-08 13:44:04 +0000

    Обновила Addresses (чек-лист)

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,15 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
+        <div className='d-inline-flex flex-column justify-content-between p-4 gap-2 mt-4 border rounded-3'>
+            <div>
+                <div className='title'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-3'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>                
+                </div>
+            </div>            
+            <PrimaryButton>Добавить филиал</PrimaryButton>
         </div>
     );
 }
--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -9,7 +9,7 @@ const Option = styled.div``;
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
+            <Option className='flex-grow-1 py-3'>{props.children}</Option>
             <div className='d-flex gap-1'>
                 <IconOnlyButton icon={<EditIcon/>}/>
                 <IconOnlyButton icon={<RemoveIcon/>}/>
